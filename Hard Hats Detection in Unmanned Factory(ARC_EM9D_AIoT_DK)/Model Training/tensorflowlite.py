b1f463f3913f9cd995d0b73fb7515cefdf9df93b
wildman8606 <yumingchang@itri.org.tw>
2021-12-30 10:56:04 +0800

    更改名稱

--- Hard Hats Detection in Unmanned Factory(ARC_EM9D_AIoT_DK)/Model Training/tensorflowlite.py
+++ Hard Hats Detection in Unmanned Factory(ARC_EM9D_AIoT_DK)/Model Training/tensorflowlite.py
@@ -15,17 +15,25 @@ config.gpu_options.per_process_gpu_memory_fraction=0.9  #分配百分之90
 config.gpu_options.allow_growth = True   #按需分配顯存，重要
 session = InteractiveSession(config=config)
 
+from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense, Dropout,MaxPooling2D
-from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPool2D, Flatten, Dense, Dropout, MaxPooling2D, Activation
+from tensorflow.keras.models import Model
 from tensorflow.keras.models import load_model
+from tensorflow.keras import models
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
 import random
 import cv2
+import os
 from tensorflow.keras import optimizers
 import csv
+import math
+import tensorflow as tf 
 
 #Settng #Settng #Settng
 epochs = 10000
@@ -33,43 +41,7 @@ nb_classes = 2
 batch_size = 32
 img_width = 32
 img_height = 32
-train_data_dir = './'
-valid_data_dir = './'
-
-
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Activation
-from tensorflow.keras.layers import AveragePooling2D, Input, Flatten
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras.callbacks import ReduceLROnPlateau
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras import backend as K
-from tensorflow.keras.models import Model
-import numpy as np
-import pandas as pd
-import math
-import tensorflow as tf 
-from tensorflow.keras import models
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM, GRU, Input
-
-
-'''
-import matplotlib.image as mpimg
-from matplotlib.font_manager import FontProperties
-plt.rcParams['font.sans-serif']=['Microsoft JhengHei']
-plt.rcParams['axes.unicode_minus']=False   
-'''
-
-import os
-import numpy as np
-
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+train_data_dir = '../Training Data/'
 
 #datagen = ImageDataGenerator(validation_split=0.1)
 datagen = ImageDataGenerator()
@@ -83,10 +55,6 @@ train_generator =datagen.flow_from_directory(directory=train_data_dir,
     batch_size=batch_size,
     subset='training')
 
-#int8   : -128 ~ 127
-
-
-
 x_train=np.concatenate([train_generator.next()[0] for i in range(train_generator.__len__())])
 y_train=np.concatenate([train_generator.next()[1] for i in range(train_generator.__len__())])
 print(x_train.shape)
@@ -95,8 +63,8 @@ print(y_train.shape)
 x_train = x_train.astype(np.float32)
 y_train = y_train.astype(np.float32)
 
-
 # Normalize
+#int8   : -128 ~ 127
 '''
 def thinning(image):
     tmp = np.where(image < 210.0, 0, image)
@@ -104,7 +72,6 @@ def thinning(image):
 
 x_train = thinning(x_train)
 '''
-
 
 x_train = (x_train - 128.0) / 128.0
 
@@ -114,14 +81,10 @@ x_train = x_train.astype(np.int8)
 y_train = y_train.astype(np.int8)
 print(np.info(x_train))
 '''
-
 print(x_train.shape)
 print(y_train.shape)
 
-
-
 # step-2 : build model
-
 model =Sequential()
 
 model.add(Conv2D(8,(3,3), input_shape=(img_width, img_height, 1)))
@@ -149,8 +112,6 @@ earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=7, verbose=0
 
 callbacks = [lr_reducer, earlystop]
 
-
-
 print('starting training....')
 history = model.fit(x_train,y_train,
                   batch_size=batch_size,
@@ -165,7 +126,6 @@ print('saving models.h5')
 model.save_weights('models.h5')
 
 #TFLiteConverter
-
 image_shape = (img_width, img_width, 1)
 def representative_dataset_gen():
     num_calibration_images = 2
@@ -203,5 +163,3 @@ tf.compat.v1.reset_default_graph()
 print('------- 結束 -------')
 '''
 '''
-
-
